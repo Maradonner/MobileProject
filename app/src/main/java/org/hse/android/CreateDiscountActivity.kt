d53654e98c9f0317e26041a74b90e675521082be
Sergey Kiryanov <aappkkaa@gmail.com>
2024-05-20 15:44:24 +0500

    Сделал добавление скидки, простое, надо доделать проверку ссылки на товар + сделать добавление изображения в БД (сейчас хардкодом ссылка на одну и ту же картинку)

--- app/src/main/java/org/hse/android/CreateDiscountActivity.kt
+++ app/src/main/java/org/hse/android/CreateDiscountActivity.kt
@@ -1,30 +1,37 @@
 package org.hse.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import models.Discount
+import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import services.AuthInterceptor
 import services.TokenManager
+
 
 class CreateDiscountActivity : AppCompatActivity() {
     private lateinit var editTextTitle: EditText
     private lateinit var editTextDescription: EditText
     private lateinit var editTextDiscountLink: EditText
-    private lateinit var editTextImageLink: EditText
+    //private lateinit var editTextImageLink: EditText
     private lateinit var editTextDefaultPrice: EditText
     private lateinit var editTextDiscountPrice: EditText
     private lateinit var buttonSubmit: Button
+    private lateinit var buttonAddImage: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,10 +40,11 @@ class CreateDiscountActivity : AppCompatActivity() {
         editTextTitle = findViewById(R.id.editTextTitle)
         editTextDescription = findViewById(R.id.editTextDescription)
         editTextDiscountLink = findViewById(R.id.editTextDiscountLink)
-//        editTextImageLink = findViewById(R.id.editTextImageLink)
-//        editTextDefaultPrice = findViewById(R.id.editTextDefaultPrice)
-//        editTextDiscountPrice = findViewById(R.id.editTextDiscountPrice)
-//        buttonSubmit = findViewById(R.id.buttonSubmit)
+        //editTextImageLink = findViewById(R.id.editTextImageLink)
+        editTextDefaultPrice = findViewById(R.id.editTextDefaultPrice)
+        editTextDiscountPrice = findViewById(R.id.editTextDiscountPrice)
+        buttonSubmit = findViewById(R.id.buttonSubmit)
+        buttonAddImage = findViewById(R.id.buttonAddImage)
 
         buttonSubmit.setOnClickListener {
             performCreationDiscount()
@@ -47,7 +55,7 @@ class CreateDiscountActivity : AppCompatActivity() {
         val title = editTextTitle.text.toString().trim()
         val description = editTextDescription.text.toString().trim()
         val discountLink = editTextDiscountLink.text.toString().trim()
-        val imageLink = editTextImageLink.text.toString().trim()
+        //val imageLink = editTextImageLink.text.toString().trim()
         val defaultPrice = editTextDefaultPrice.text.toString().trim()
         val discountPrice = editTextDiscountPrice.text.toString().trim()
 
@@ -55,26 +63,30 @@ class CreateDiscountActivity : AppCompatActivity() {
             Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show()
             return
         }
+        val discount: Discount = Discount()
+        discount.title = title
+        discount.description = description
+        discount.discountLink = discountLink
+        //val imageLink = editTextImageLink.text.toString().trim()
+        discount.defaultPrice = defaultPrice.toDouble()
+        discount.discountPrice = discountPrice.toDouble()
+        discount.imageLink = "https://storage.yandexcloud.net/pictures/4db1bbfc-ef54-416f-9ec9-cf6bb4c46cc7"
+
+        val gson = Gson()
+        val json = gson.toJson(discount)
+
 
         val mediaType = "application/json".toMediaType()
-        val body = """
-            {
-              "title": "$title",
-              "description": "$description",
-              "discountLink": "$discountLink",
-              "imageLink": "$imageLink",
-              "defaultPrice": $defaultPrice,
-              "discountPrice": $discountPrice
-            }
-        """.trimIndent().toRequestBody(mediaType)
-
+        val body = json.toRequestBody("application/json; charset=utf-8".toMediaType())
+        Log.e("BODY_FIRST", json)
         CoroutineScope(Dispatchers.IO).launch {
             submitDiscount(body)
         }
     }
 
-    private suspend fun submitDiscount(body: okhttp3.RequestBody) {
+    private suspend fun submitDiscount(body: RequestBody) {
         val tokenManager = TokenManager(this@CreateDiscountActivity)
+        Log.e("BODY", body.toString())
         val client = OkHttpClient()
             .newBuilder()
             .addInterceptor(AuthInterceptor(tokenManager))
@@ -93,6 +105,7 @@ class CreateDiscountActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         Toast.makeText(this@CreateDiscountActivity, "Discount created successfully: $responseBody", Toast.LENGTH_LONG).show()
                     } else {
+                        Log.e("ErrorResponse", responseBody.toString())
                         Toast.makeText(this@CreateDiscountActivity, "Failed to create discount: $responseBody", Toast.LENGTH_LONG).show()
                     }
                 }
