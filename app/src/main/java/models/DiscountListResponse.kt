2b0173fbca1633862c85bfbeb8a7f76c1f51c113
Sergey Kiryanov <aappkkaa@gmail.com>
2024-05-20 23:26:31 +0500

    Обновил подгрузку данных в соответствии с изменениями на бэке (Discounts теперь достаются из "items"

---
+++ app/src/main/java/models/DiscountListResponse.kt
@@ -0,0 +1,11 @@
+package models
+
+class DiscountListResponse {
+    val items: List<Discount>? = null
+    val currentPage: Int? = null
+    val totalPages: Int? = null
+    val pageSize: Int? = null
+    val totalCount: Int? = null
+    val hasPrevious: Boolean? = null
+    val hasNext: Boolean? = null
+}
